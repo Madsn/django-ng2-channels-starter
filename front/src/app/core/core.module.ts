8aa2eafb8e79bb08f3f44bcb4bb54c9c97e88c3a
Mikkel Madsen <m@madsn.net>
2017-04-18 21:59:51 +0400

    Bootstrap navbar

--- front/src/app/core/core.module.ts
+++ front/src/app/core/core.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { Ng2DjangoChannelsDataBindingModule, WebSocketDataBindingService } from 'ng2-django-channels-data-binding';
 import { Ng2DjangoChannelsDemultiplexingModule } from 'ng2-django-channels-demultiplexing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from '@angular/material';
 
 import { WebsocketService } from './websocket.service';
 
@@ -11,11 +10,10 @@ import { WebsocketService } from './websocket.service';
   imports: [
     CommonModule,
     BrowserAnimationsModule,
-    MaterialModule.forRoot(),
     Ng2DjangoChannelsDataBindingModule,
     Ng2DjangoChannelsDemultiplexingModule.forRoot({websocket_url: 'ws://127.0.0.1:8000/api/ws'})
   ],
   providers: [WebsocketService],
-  exports: [BrowserAnimationsModule, MaterialModule]
+  exports: [BrowserAnimationsModule]
 })
 export class CoreModule { }
